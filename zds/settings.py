0c3f8c9ed1bbb5dd610ea632170ece5d0b72fa14
Gerard Paligot <gerard.paligot@gmail.com>
2015-03-08 10:26:49 +0100

    feat(news): List all news.
    Creates the new module news with the model News and a view to
    list all news pagined.

--- zds/settings.py
+++ zds/settings.py
@@ -188,6 +188,7 @@ INSTALLED_APPS = (
     'zds.forum',
     'zds.tutorial',
     'zds.member',
+    'zds.news',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -464,6 +465,9 @@ ZDS_APP = {
         'max_post_length': 1000000,
         'top_tag_max': 5,
         'home_number': 5,
+    },
+    'news': {
+        'news_per_page': 100,
     },
     'paginator': {
         'folding_limit': 4
