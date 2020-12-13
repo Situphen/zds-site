32fe656a325bf525c84d1a44f8067fd77e47c849
Situphen <situphen@gmail.com>
2020-12-14 00:24:32 +0100

    (SocialAuth) Un seul compte peu importe le moyen de connexion

--- zds/settings/abstract_base/requirements.py
+++ zds/settings/abstract_base/requirements.py
@@ -20,6 +20,7 @@ SOCIAL_AUTH_PIPELINE = (
     "social.pipeline.social_auth.auth_allowed",
     "social.pipeline.social_auth.social_user",
     "social.pipeline.user.get_username",
+    "social.pipeline.social_auth.associate_by_email",
     "social.pipeline.user.create_user",
     "zds.member.models.save_profile",
     "social.pipeline.social_auth.associate_user",
